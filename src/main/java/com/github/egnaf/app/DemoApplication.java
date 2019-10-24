c2ec8ca429e4f2fa6fc05d940fddcad397138148
egnaf <mamadaliev@yahoo.com>
2019-10-24 04:57:46 +0300

    Add DemoApplication

---
+++ src/main/java/com/github/egnaf/app/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.github.egnaf.app;
+
+import com.github.egnaf.app.service.HumanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    private final HumanService humanService;
+
+    @Autowired
+    public DemoApplication(HumanService humanService) {
+        this.humanService = humanService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public void fillHumansTable() {
+        humanService.create("maeshi", 19);
+        humanService.create("hardelele", 20);
+        humanService.create("varkrift", 20);
+    }
+}
