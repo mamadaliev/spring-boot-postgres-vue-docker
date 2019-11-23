926c4df0f7e73b9e723d32a20e2cfc970148a2ce
egnaf <mamadaliev@yahoo.com>
2019-11-23 17:16:29 +0300

    Add existsByEmail and existsByNickname methods

--- backend/src/main/java/com/github/egnaf/spring_boot_docker_example/repository/UserRepository.java
+++ backend/src/main/java/com/github/egnaf/spring_boot_docker_example/repository/UserRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 }
