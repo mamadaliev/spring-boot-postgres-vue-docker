4dcb89d4062c75a3c15187f346532a98601cf957
egnaf <mamadaliev@yahoo.com>
2019-11-23 16:47:26 +0300

    Rename class name student to user

--- backend/src/main/java/com/github/egnaf/spring_boot_docker_example/domain/User.java
+++ backend/src/main/java/com/github/egnaf/spring_boot_docker_example/domain/User.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Student {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
