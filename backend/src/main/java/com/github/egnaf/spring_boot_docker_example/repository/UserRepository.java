9572aecc9ff35f9cfd338662b7a86a52c6979b60
egnaf <mamadaliev@yahoo.com>
2019-11-23 16:47:59 +0300

    Add user repository interface that extends jpa repository class

---
+++ backend/src/main/java/com/github/egnaf/spring_boot_docker_example/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.github.egnaf.spring_boot_docker_example.repository;
+
+import com.github.egnaf.spring_boot_docker_example.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
