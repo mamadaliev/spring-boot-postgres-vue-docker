3405f2b9894b5ce15bb0452ddd4dd4bac6c579b9
Sherzod Mamadaliev <egnaf@yahoo.com>
2019-11-27 22:48:30 +0300

    Add error handler and fix user controller #34
    * Fix devtools in pom.xml
    * Fix user service and controller
    * Add controller advice and error response class

---
+++ backend/src/main/java/com/github/egnaf/spring_boot_docker_example/rest/error/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.github.egnaf.spring_boot_docker_example.rest.error;
+
+import lombok.Data;
+
+@Data
+public class ErrorResponse {
+    private int status;
+    private String message;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
